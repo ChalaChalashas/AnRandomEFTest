7714608c36634817c7590392b22c279f8f2daa42
ChalaChalashas <p.kaunietis@gmail.com>
2023-06-11 21:04:56 +0300

    mapping changes .ForMember

--- AnFolderApi/FolderMappingProfile.cs
+++ AnFolderApi/FolderMappingProfile.cs
@@ -6,7 +6,8 @@ public class FolderMappingProfile : Profile
 {
     public FolderMappingProfile()
     {
-        CreateMap<FolderEntity, FolderDto>();
+        CreateMap<FolderEntity, FolderDto>()
+            .ForMember(d => d.Children, o => o.MapFrom(s => s.Children));
         CreateMap<FolderDto, FolderEntity>();
     }
 }
